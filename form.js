bf1601248a7f810aeaff2762965c07cc4b0d46d7
palenquefiestasdeoctubre <infopalenquefiestasdeoctubre@gmail.com>
2025-06-16 21:02:15 -0600

    Add files via upload

---
+++ form.js
@@ -0,0 +1,5 @@
+
+document.getElementById('form-compra').addEventListener('submit', function(e) {
+    e.preventDefault();
+    alert('Formulario enviado. (Aquí se configurará el envío automático de correo)');
+});
